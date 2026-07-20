b173a20f46830f8b4ca5af49ea3026d391698b1a
adaml <adam-lundrigan@proton.me>
2026-07-20 13:30:49 -0600

    Rewrite of Input module. - Removed `bevy_mod_gba` approach. - Instead, analyze bitmask input for each category (held, down, up). - Removed OgcStick trait in favour of OgcSticks struct to track stick   values. - Added local param to track OgcSticks values. - Removed Wii button mappings in pad_to_bevy_button function.

--- src/input.rs
+++ src/input.rs
@@ -12,195 +12,153 @@ impl Plugin for OgcInputPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PreUpdate, update_controllers);
     }
+    fn finish(&self, app: &mut App) {
+        // Initialize controllers.
+        Input::init(ControllerType::Gamecube);
+        Input::init(ControllerType::Wii);
+    }
 }
 
+// TODO: Rumble?
 /// Check the buttons and stick inputs from all controllers.
-/// This system runs in the PreUpdate schedule, and reads inputs from
-/// the OGC controller before then writing them to every Gamepad in
-/// Bevy for use in the Update schedule.
-/// TODO: Rumble?
+/// This system runs in the PreUpdate schedule, and reads inputs from the OGC controller before writing them to their respective Bevy Gamepads for use in systems in the Update schedule.
 fn update_controllers(
     controller_q: Query<(Entity, &OgcController)>,
     mut events: MessageWriter<RawGamepadEvent>,
     mut button_events: MessageWriter<RawGamepadButtonChangedEvent>,
     mut stick_events: MessageWriter<RawGamepadAxisChangedEvent>,
+    mut prev_stick_values: Local<OgcSticks>,
 ) {
     Input::update(ControllerType::Gamecube);
     Input::update(ControllerType::Wii);
 
-    let input_flags: Vec<Button> = vec![
-        Button::Left,
-        Button::Right,
-        Button::Up,
-        Button::Down,
-        Button::TrigL,
-        Button::TrigR,
-        Button::TrigZ,
-        Button::TrigZL,
-        Button::TrigZR,
-        Button::A,
-        Button::B,
-        Button::C,
-        Button::X,
-        Button::Y,
-        Button::Z,
-        Button::One,
-        Button::Two,
-        Button::Minus,
-        Button::Plus,
-        Button::Home,
-        Button::Start,
-    ];
-
     for (entity, controller) in controller_q.iter() {
         // Process the button inputs for each controller.
-        // TODO: Currently alter the upstream crate by deriving `Clone` and `Copy` on Button. Will have to see about upstreaming.
-        input_flags
-            .iter()
-            .filter_map(ogc_to_bevy_button)
-            .filter_map(|(ogc_button, bevy_button)| {
-                controller
-                    .0
-                    .is_button_held(ogc_button)
-                    .then_some(1.)
-                    .or(controller.0.is_button_up(ogc_button).then_some(0.))
-                    .map(|value| RawGamepadButtonChangedEvent::new(entity, bevy_button, value))
-            })
-            .for_each(|event| {
+        let held_buttons = controller.0.as_pad().buttons_held();
+        let down_buttons = controller.0.as_pad().buttons_down();
+        let up_buttons = controller.0.as_pad().buttons_up();
+
+        // Iterate over all buttons returned in a category.
+        held_buttons.iter().for_each(|button| {
+            // Check to see the button will convert to a Bevy GamepadButton.
+            if let Some(bevy_button) = pad_to_bevy_button(&button) {
+                // Write the button event
+                let event = RawGamepadButtonChangedEvent::new(entity, bevy_button, 1.);
                 events.write(event.into());
                 button_events.write(event);
-            });
+            }
+        });
+        down_buttons.iter().for_each(|button| {
+            if let Some(bevy_button) = pad_to_bevy_button(&button) {
+                let event = RawGamepadButtonChangedEvent::new(entity, bevy_button, 1.);
+                events.write(event.into());
+                button_events.write(event);
+            }
+        });
+        up_buttons.iter().for_each(|button| {
+            if let Some(bevy_button) = pad_to_bevy_button(&button) {
+                let event = RawGamepadButtonChangedEvent::new(entity, bevy_button, 0.);
+                events.write(event.into());
+                button_events.write(event);
+            }
+        });
 
         // Check if there's input to be read from the controller sticks.
-        // TODO: Has to be a better way to do this, lol.
-        if let Some(left_stick_x_input) = controller.as_pad().to_bevy_stick(GamepadAxis::LeftStickX)
-        {
+        let stick_values = OgcSticks::read(&controller);
+
+        // Check if the stick value have changed.
+        if stick_values.main_x != prev_stick_values.main_x {
+            // Create a new event if the value has changed.
             let left_stick_x_event = RawGamepadAxisChangedEvent::new(
                 entity,
                 GamepadAxis::LeftStickX,
-                left_stick_x_input,
+                stick_values.main_x,
             );
+            // Write the event to the event queues.
             events.write(left_stick_x_event.into());
             stick_events.write(left_stick_x_event);
+            // Update the local stick value.
+            prev_stick_values.main_x = stick_values.main_x;
         }
-        if let Some(left_stick_y_input) = controller.as_pad().to_bevy_stick(GamepadAxis::LeftStickX)
-        {
+        if stick_values.main_y != prev_stick_values.main_y {
             let left_stick_y_event = RawGamepadAxisChangedEvent::new(
                 entity,
-                GamepadAxis::RightStickY,
-                left_stick_y_input,
+                GamepadAxis::LeftStickY,
+                stick_values.main_y,
             );
             events.write(left_stick_y_event.into());
             stick_events.write(left_stick_y_event);
+            prev_stick_values.main_y = stick_values.main_y;
         }
-        if let Some(right_stick_x_input) =
-            controller.as_pad().to_bevy_stick(GamepadAxis::RightStickX)
-        {
-            let right_stick_x_event = RawGamepadAxisChangedEvent::new(
-                entity,
-                GamepadAxis::RightStickX,
-                right_stick_x_input,
-            );
+        if stick_values.c_x != prev_stick_values.c_x {
+            let right_stick_x_event =
+                RawGamepadAxisChangedEvent::new(entity, GamepadAxis::RightStickX, stick_values.c_x);
             events.write(right_stick_x_event.into());
             stick_events.write(right_stick_x_event);
+            prev_stick_values.c_x = stick_values.c_x;
         }
-        if let Some(right_stick_y_input) =
-            controller.as_pad().to_bevy_stick(GamepadAxis::RightStickY)
-        {
-            let right_stick_y_event = RawGamepadAxisChangedEvent::new(
-                entity,
-                GamepadAxis::RightStickY,
-                right_stick_y_input,
-            );
+        if stick_values.c_y != prev_stick_values.c_y {
+            let right_stick_y_event =
+                RawGamepadAxisChangedEvent::new(entity, GamepadAxis::RightStickY, stick_values.c_y);
             events.write(right_stick_y_event.into());
             stick_events.write(right_stick_y_event);
+            prev_stick_values.c_y = stick_values.c_y;
         }
     }
 }
 
 /// Helper component to access the Inputs from ogc_rs.
+///
+/// A new entity with this component should be created for every controller activated.
 #[derive(Component, Deref, DerefMut)]
 pub struct OgcController(ogc_rs::input::Input);
 
 impl OgcController {
     #[must_use]
-    pub fn new(controller_type: ControllerType, port: i8) -> Self {
-        let port_num = match port {
-            0 => ControllerPort::One,
-            1 => ControllerPort::Two,
-            2 => ControllerPort::Three,
-            3 => ControllerPort::Four,
-            _ => ControllerPort::One,
-        };
-        Self(ogc_rs::input::Input::new(controller_type, port_num))
+    pub fn new(controller_type: ControllerType, port: ControllerPort) -> Self {
+        Self(ogc_rs::input::Input::new(controller_type, port))
     }
 }
 
-trait OgcStick {
-    // Conversion from Ogc Stick to a Bevy Stick
-    fn to_bevy_stick(&self, stick: GamepadAxis) -> Option<f32>;
+#[derive(Default)]
+struct OgcSticks {
+    main_x: f32,
+    main_y: f32,
+    c_x: f32,
+    c_y: f32,
 }
 
-impl OgcStick for ogc_rs::input::Pad {
-    fn to_bevy_stick(&self, stick: GamepadAxis) -> Option<f32> {
-        use bevy::input::gamepad::GamepadAxis;
-
-        match stick {
-            GamepadAxis::LeftStickX => {
-                if self.stick_x() == 0 {
-                    return None;
-                }
-                return Some(self.stick_y() as f32);
-            }
-            GamepadAxis::LeftStickY => {
-                if self.stick_y() == 0 {
-                    return None;
-                }
-                return Some(self.stick_y() as f32);
-            }
-            GamepadAxis::RightStickX => {
-                if self.c_stick_x() == 0 {
-                    return None;
-                }
-                return Some(self.c_stick_y() as f32);
-            }
-            GamepadAxis::RightStickY => {
-                if self.c_stick_y() == 0 {
-                    return None;
-                }
-                return Some(self.c_stick_y() as f32);
-            }
-            _ => return None,
+impl OgcSticks {
+    /// Reads the stick values from the controller and returns a [`OgcSticks`] struct with values compatible with Bevy's [`GamepadAxis`].
+    pub fn read(controller: &OgcController) -> Self {
+        let pad = controller.0.as_pad();
+        Self {
+            main_x: (pad.stick_x() as f32) / (100.0),
+            main_y: (pad.stick_y() as f32) / (100.0),
+            c_x: (pad.c_stick_x() as f32) / (100.0),
+            c_y: (pad.c_stick_y() as f32) / (100.0),
         }
     }
 }
 
-const fn ogc_to_bevy_button(
-    button: &ogc_rs::input::Button,
-) -> Option<(ogc_rs::input::Button, bevy::input::gamepad::GamepadButton)> {
-    use ogc_rs::input::Button;
-
-    match button {
-        Button::Left => Some((Button::Left, GamepadButton::DPadLeft)),
-        Button::Right => Some((Button::Right, GamepadButton::DPadRight)),
-        Button::Up => Some((Button::Up, GamepadButton::DPadUp)),
-        Button::Down => Some((Button::Down, GamepadButton::DPadDown)),
-        Button::TrigL => Some((Button::TrigL, GamepadButton::LeftTrigger)),
-        Button::TrigR => Some((Button::TrigR, GamepadButton::RightTrigger)),
-        Button::TrigZ => Some((Button::TrigZ, GamepadButton::Z)),
-        Button::TrigZL => Some((Button::TrigZL, GamepadButton::LeftTrigger2)),
-        Button::TrigZR => Some((Button::TrigZR, GamepadButton::RightTrigger2)),
-        Button::A => Some((Button::A, GamepadButton::East)),
-        Button::B => Some((Button::B, GamepadButton::South)),
-        Button::C => Some((Button::C, GamepadButton::C)),
-        Button::X => Some((Button::X, GamepadButton::North)),
-        Button::Y => Some((Button::Y, GamepadButton::West)),
-        Button::Z => Some((Button::Z, GamepadButton::Z)),
-        Button::One => Some((Button::One, GamepadButton::Other(0))),
-        Button::Two => Some((Button::Two, GamepadButton::Other(1))),
-        Button::Minus => Some((Button::Minus, GamepadButton::Other(2))),
-        Button::Plus => Some((Button::Plus, GamepadButton::Other(3))),
-        Button::Home => Some((Button::Home, GamepadButton::Mode)),
-        Button::Start => Some((Button::Start, GamepadButton::Start)),
+// TODO: How to work in Wii controller and Gamecube Controller variants?
+/// Converts the bit values of an ogc-rs [`PadButton`] to a Bevy [`GamepadButton`].
+const fn pad_to_bevy_button(
+    button: &ogc_rs::input::pad::PadButton,
+) -> Option<bevy::input::gamepad::GamepadButton> {
+    match button.bits() {
+        0x0001 => Some(GamepadButton::DPadLeft),
+        0x0002 => Some(GamepadButton::DPadRight),
+        0x0004 => Some(GamepadButton::DPadDown),
+        0x0008 => Some(GamepadButton::DPadUp),
+        0x0010 => Some(GamepadButton::Z),
+        0x0020 => Some(GamepadButton::RightTrigger),
+        0x0040 => Some(GamepadButton::LeftTrigger),
+        0x0100 => Some(GamepadButton::South), // PAD_BUTTON_A
+        0x0200 => Some(GamepadButton::West),  // PAD_BUTTON_B
+        0x0400 => Some(GamepadButton::East),  // PAD_BUTTON_Y
+        0x0800 => Some(GamepadButton::North), // PAD_BUTTON_X
+        0x1000 => Some(GamepadButton::Start),
+        _ => None,
     }
 }
